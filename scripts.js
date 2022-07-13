e48fb9cf7258c1e3d5d0154d62d2ed93dab43b35
vikarosemary <109240842+vikarosemary@users.noreply.github.com>
2022-07-13 21:14:11 +0300

    init

---
+++ scripts.js
@@ -0,0 +1,31 @@
+$('.gamburger').click(function(){
+    $('span:nth-child(1)').toggleClass('first');
+    $('span:nth-child(2)').toggleClass('middle');
+    $('span:nth-child(3)').toggleClass('last');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
